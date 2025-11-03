ba4cbe10453134b907ac935b052b3712af0f978c
doyeon-SM <82075191+doyeon-SM@users.noreply.github.com>
2025-11-04 00:58:55 +0900

    25.11.04
    프레임 고정(30)
    프레임 관련 설정 수정(빌드, 플레이)
    vfx 폴더 이동

---
+++ Assets/Scripts/BuildSetting/FrameRateLimiter.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FrameRateLimiter : MonoBehaviour
+{
+    void Awake()
+    {
+        // 화면이 꺼지지 않게 방지 (선택 사항)
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        // VSync 비활성화
+        QualitySettings.vSyncCount = 0;
+
+        // 프레임 고정
+        Application.targetFrameRate = 30;
+    }
+}
